6161196c0fd17c8768385e449a56165bca50cf14
Josué Castro <josuecastro86@gmail.com>
2024-07-05 14:26:36 -0600

    chore: Added Input System
    - Added Input System Package
    - Created Input System Controller with Keyboar and Mouse Schema and Gamepad schema
    - Added input events for Move, Look, Jump, Sprint, Crouch, Interact, Aim and Attack
    - Added Player Input to PlayerCharacterController
    - Edited Script PlayerInputManager to use the Input System
    - Edited PlayerLookController to determine the update of the look Vector2 depending on the Input Schema (Mouse or Gamepad Right Stick)
    - Values that need to be read constantly are using subscription to C# events
    - Values that are triggered are read in the Update function

--- Assets/PlayerCharacterController/Scripts/PlayerInputManager.cs
+++ Assets/PlayerCharacterController/Scripts/PlayerInputManager.cs
@@ -1,25 +1,87 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputManager : MonoBehaviour
 {
+    private PlayerInput playerInput;
+
+    [HideInInspector] public InputAction moveAction;
+    [HideInInspector] public InputAction lookAction;
+    [HideInInspector] public InputAction jumpAction;
+    [HideInInspector] public InputAction sprintAction;
+    [HideInInspector] public InputAction crouchAction;
+    [HideInInspector] public InputAction interactAction;
+
+    [HideInInspector] public InputAction aimAction;
+
+    [HideInInspector] public InputAction attackAction;
+
     public Vector2 move;
     public Vector2 look;
     public bool jump;
     public bool sprint;
+    public bool crouch;
 
     public bool aim;
+    public bool attack;
+
+    public bool isGamepad;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+
+        moveAction = playerInput.actions["Move"];
+        lookAction = playerInput.actions["Look"];
+        jumpAction = playerInput.actions["Jump"];
+        sprintAction = playerInput.actions["Sprint"];
+        crouchAction = playerInput.actions["Crouch"];
+        interactAction = playerInput.actions["Interact"];
+        aimAction = playerInput.actions["Aim"];
+        attackAction = playerInput.actions["Attack"];
+    }
+
+    private void OnEnable()
+    {
+        moveAction.performed += ctx => move = ctx.ReadValue<Vector2>();
+        moveAction.canceled += ctx => move = ctx.ReadValue<Vector2>();
+
+        lookAction.performed += ctx => look = ctx.ReadValue<Vector2>();
+        lookAction.canceled += ctx => look = ctx.ReadValue<Vector2>();
+
+        sprintAction.performed += ctx => jump = true;
+        sprintAction.canceled += ctx => jump = false;
+
+        aimAction.performed += ctx => aim = true;
+        aimAction.canceled += ctx => aim = false;
+
+        attackAction.performed += ctx => attack = true;
+        attackAction.canceled += ctx => attack = false;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.performed -= ctx => move = ctx.ReadValue<Vector2>();
+        moveAction.canceled -= ctx => move = ctx.ReadValue<Vector2>();
+
+        lookAction.performed -= ctx => look = ctx.ReadValue<Vector2>();
+        lookAction.canceled -= ctx => look = ctx.ReadValue<Vector2>();
+
+        sprintAction.performed -= ctx => jump = true;
+        sprintAction.canceled -= ctx => jump = false;
+
+        aimAction.performed -= ctx => aim = true;
+        aimAction.canceled -= ctx => aim = false;
+
+        attackAction.performed -= ctx => attack = true;
+        attackAction.canceled -= ctx => attack = false;
+    }
 
     private void Update()
     {
-        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        move = Vector2.ClampMagnitude(move, 1f);
+        jump = jumpAction.triggered;
+        crouch = crouchAction.triggered;
 
-        look = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-
-        jump = Input.GetKeyDown(KeyCode.Space);
-
-        sprint = Input.GetKey(KeyCode.LeftShift);
-
-        aim = Input.GetKey(KeyCode.Mouse1);
+        isGamepad = playerInput.currentControlScheme == "Gamepad";
     }
 }
